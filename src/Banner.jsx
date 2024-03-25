ea1fe7629ceb1a18ea89de42a41ffefe636c8836
israt.js <israt8291jahan@gmail.com>
2024-03-25 22:22:36 +0600

    add nav

---
+++ src/Banner.jsx
@@ -0,0 +1,14 @@
+const Banner = () => {
+    return(
+        <div className="flex">
+            <div className="text">
+                <h2 className="text-white">Books to freshen up your bookshelf</h2>
+                <button className="">View The List</button>
+            </div>
+            <div className="">
+                <img src="banner.png" alt="" />
+            </div>
+        </div>
+    )
+}
+export default Banner;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Banner from './Banner.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/banner",
+    element: <Banner></Banner>
+  },
+  Children
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
